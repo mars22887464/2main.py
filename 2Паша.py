2418cafa30978c862d51a76968e648a59386502f
mars22887464 <117431512+mars22887464@users.noreply.github.com>
2022-11-24 22:55:52 +1000

    Add files via upload

---
+++ 2Паша.py
@@ -0,0 +1,10 @@
+a=input ('What is your name?')
+b=input("How old are you? ")
+c=input("Where are you live?")
+print("This is ", a)
+print("It is ", b)
+print("(S)he live ib ",c)
+a = "Раз"
+b = "Два"
+с = "Три"
+print(a,b,с,sep='*')
